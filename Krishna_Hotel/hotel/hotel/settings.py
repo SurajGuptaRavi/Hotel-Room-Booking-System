b5c91eb57456b4e9199c6465563ac5dc31c474e6
Suraj Rajesh Gupta <61044439+SurajGuptaRavi@users.noreply.github.com>
2020-08-09 02:10:51 +0530

    Update settings.py

--- Krishna_Hotel/hotel/hotel/settings.py
+++ Krishna_Hotel/hotel/hotel/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cw24t3huxtgm3t6i=y$zbkkw1pml3r!++5xt7_re0z&k6y-%j$'
+SECRET_KEY = 'cafusabcdefghiacfdeysaczdsfoapqwsbbbbbbbbbbbsaaaaaaaaaaa' #this is the dummy key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
